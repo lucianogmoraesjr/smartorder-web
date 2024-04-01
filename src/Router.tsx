987968d26072e2d841688f76eb5573463da6e197
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-01 19:07:19 -0300

    feat: add react router dom

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { Products } from './pages/Products';
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
