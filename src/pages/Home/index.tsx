e55308553dc3485bee57542c53c59d3d4fdbb81a
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-01 21:01:39 -0300

    feat: make header component reusable

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '../../components/Header';
+import HomeIcon from '../../components/Icons/HomeIcon';
 import { Orders } from '../../components/Orders';
 
 import { Container } from './styles';
@@ -6,7 +7,11 @@ import { Container } from './styles';
 export function Home() {
   return (
     <Container>
-      <Header />
+      <Header
+        title="Home"
+        subtitle="Acompanhe os pedidos dos clientes"
+        icon={HomeIcon}
+      />
       <Orders />
     </Container>
   );
