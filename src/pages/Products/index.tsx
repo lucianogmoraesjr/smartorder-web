0d389dbdf748c366b495fc0c85b9ecada2a68195
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-01 21:05:56 -0300

    feat: add header in products page

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,8 +1,16 @@
+import { Header } from '../../components/Header';
+import MenuIcon from '../../components/Icons/MenuIcon';
+
 import { Container } from './styles';
 
 export function Products() {
   return (
     <Container>
+      <Header
+        title="Cardápio"
+        subtitle="Gerencie os produtos do seu estabelecimento"
+        icon={MenuIcon}
+      />
       <h1>Products</h1>
     </Container>
   );
