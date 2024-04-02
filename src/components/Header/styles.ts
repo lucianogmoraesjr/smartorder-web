d823d6b25f71c91db35f77a480024ce3b85a88b0
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-02 11:18:44 -0300

    feat: products table

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,6 +6,7 @@ export const Container = styled.header`
   align-items: center;
   height: 4.5rem;
   margin-top: 2.5rem;
+  margin-bottom: 3rem;
 `;
 
 export const Content = styled.div`
--- src/pages/Products/styles.ts
+++ src/pages/Products/styles.ts
@@ -1,3 +1,139 @@
 import { styled } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  padding-right: 5rem;
+`;
+
+export const NavBar = styled.nav`
+  display: flex;
+  width: 100%;
+  border-bottom: 1px solid rgba(204, 204, 204, 0.4);
+
+  a {
+    text-decoration: none;
+    font-size: 0.875rem;
+    line-height: 1.5;
+    color: #666;
+    padding: 1rem 2.5rem;
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
+
+    &.active {
+      color: #d73035;
+      font-weight: 600;
+      background: #fff;
+    }
+  }
+`;
+
+export const ProductsContainer = styled.main`
+  margin-top: 2rem;
+
+  .products-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    > div {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+
+      span {
+        font-weight: 400;
+      }
+    }
+
+    > button {
+      all: unset;
+      color: #d73035;
+      font-size: 0.875rem;
+      font-weight: 600;
+      line-height: 1.5;
+      cursor: pointer;
+    }
+  }
+`;
+
+export const ProductsTable = styled.table`
+  width: 100%;
+  margin-top: 1rem;
+  border-spacing: 0;
+
+  thead {
+    text-align: left;
+    background-color: rgba(204, 204, 204, 0.2);
+
+    th {
+      padding: 1rem;
+      border-top: 1px solid rgba(204, 204, 204, 0.4);
+
+      &:first-child {
+        border-left: 1px solid rgba(204, 204, 204, 0.4);
+        border-top-left-radius: 0.5rem;
+      }
+
+      &:last-child {
+        border-right: 1px solid rgba(204, 204, 204, 0.4);
+        border-top-right-radius: 0.5rem;
+        width: 6rem;
+      }
+    }
+  }
+
+  tbody {
+    td {
+      padding: 1rem;
+      border-bottom: 1px solid rgba(204, 204, 204, 0.4);
+
+      &:first-child {
+        border-left: 1px solid rgba(204, 204, 204, 0.4);
+      }
+
+      &:last-child {
+        border-right: 1px solid rgba(204, 204, 204, 0.4);
+        width: 6rem;
+      }
+    }
+
+    tr:last-child td:first-child {
+      border-bottom-left-radius: 0.5rem;
+    }
+
+    tr:last-child td:last-child {
+      border-bottom-right-radius: 0.5rem;
+    }
+
+    .actions {
+      display: flex;
+      align-items: center;
+      gap: 2rem;
+
+      svg {
+        width: 1.5rem;
+        height: 1.5rem;
+      }
+
+      a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      button {
+        all: unset;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+      }
+    }
+  }
+
+  img {
+    width: 48px;
+    height: 32px;
+    border-radius: 0.25rem;
+  }
+`;
