8e712052d6748fabd50493045230f5256f7ea84f
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-02 13:30:19 -0300

    feat: create react portal component

---
+++ src/components/ReactPortal.ts
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import ReactDOM from 'react-dom';
+
+interface ReactPortalProps {
+  containerId: string;
+  children: ReactNode;
+}
+
+export function ReactPortal({ containerId, children }: ReactPortalProps) {
+  let container = document.getElementById(containerId);
+
+  if (!container) {
+    container = document.createElement('div');
+    container.setAttribute('id', containerId);
+    document.body.appendChild(container);
+  }
+
+  return ReactDOM.createPortal(children, container);
+}
