a389c26d8e570adf3adb5551f7d2bdc269e4124f
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-08 17:37:28 -0300

    feat: create default theme

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,22 +1,26 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ThemeProvider } from 'styled-components';
 
 import { Router } from '../../Router';
 import { GlobalStyles } from '../../styles/GlobalStyles';
+import defaultTheme from '../../styles/themes/default';
 import { Sidebar } from '../Sidebar';
 
 import { Container } from './styles';
 
 export function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <Container>
-        <Sidebar />
-        <Router />
-      </Container>
-      <ToastContainer position="bottom-center" />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Container>
+          <Sidebar />
+          <Router />
+        </Container>
+        <ToastContainer position="bottom-center" />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,25 @@
+export default {
+  colors: {
+    background: '#fafafa',
+    orange: '#d76c30',
+    yellow: '#fff7ab',
+    white: '#ffffff',
+
+    gray: {
+      100: '#fafafa',
+      200: '#cccccc',
+      300: '#999999',
+      400: '#666666',
+      500: '#333333',
+    },
+
+    red: {
+      light: '#ffabad',
+      main: '#d73035',
+      dark: '#8a1114',
+    },
+    green: {
+      main: '#30d787',
+    },
+  },
+};
